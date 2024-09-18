4c2558758e884d2f65a406a8bd724e761259d197
graphemecluster <graphemecluster@gmail.com>
2024-09-18 22:17:57 +0800

    Fix extra qualification error on compiling

--- src/rime/translation.h
+++ src/rime/translation.h
@@ -120,16 +120,17 @@ inline an<Translation> Cached(Args&&... args) {
 
 class DistinctTranslation : public Translation {
  public:
-  DistinctTranslation(an<Translation> translation, const bool combine_candidates);
+  DistinctTranslation(an<Translation> translation,
+                      const bool combine_candidates);
 
   virtual bool Next();
   virtual an<Candidate> Peek();
 
  protected:
-  bool DistinctTranslation::AlreadyHas(const pair<string, string>& candidate) const;
+  bool AlreadyHas(const pair<string, string>& candidate) const;
 
   const bool combine_candidates_;
-  set<pair<string, string> > candidate_set_;
+  set<pair<string, string>> candidate_set_;
   an<Translation> translation_;
   an<Candidate> cache_;
 };
